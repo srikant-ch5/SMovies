2aa0de15f12eaf210f468b7aff1caefd5c1d202d
sriks123 <srksriks123@gmail.com>
2020-12-28 02:46:45 +0530

    Updates

---
+++ precache-manifest.145f54f4e245a5815f3e8672d8647499.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635fd518f3431d6beb940e4847bb70f6",
+    "url": "/SMovies/index.html"
+  },
+  {
+    "revision": "b9ae7b60bf5cb3165317",
+    "url": "/SMovies/static/css/2.6b00acf2.chunk.css"
+  },
+  {
+    "revision": "b9ae7b60bf5cb3165317",
+    "url": "/SMovies/static/js/2.6c0af00e.chunk.js"
+  },
+  {
+    "revision": "461e473e946292f36a23",
+    "url": "/SMovies/static/js/main.77b5476b.chunk.js"
+  },
+  {
+    "revision": "37b94b028f17dadb85bc",
+    "url": "/SMovies/static/js/runtime-main.6a8bd71f.js"
+  }
+]);
